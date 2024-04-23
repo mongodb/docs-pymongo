feeffdaf43a15769714bb4c851ffad86ef0e2357
Mike Woofter <mike.woofter@mongodb.com>
2024-04-23 11:41:04 -0500

    formatting

--- source/includes/authentication/azure-imds.py
+++ source/includes/authentication/azure-imds.py
@@ -12,11 +12,12 @@ client = MongoClient(
 # end mongoclient
 
 # start connection string
+from pymongo import MongoClient
 
+# define properties, URI, and MongoClient
 properties = {"ENVIRONMENT": "azure", "TOKEN_RESOURCE": "<audience>"}
-
 uri = ("mongodb://<hostname>:<port>/?"
-       "username=" < Azure identity client ID >"
+       "username="<Azure identity client ID>"
        "&authMechanism=MONGODB-OIDC"
        "&authMechanismProperties=properties")
 client = MongoClient(uri)
