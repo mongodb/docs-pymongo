ef9a110e9641e43e9c15a74482720dab9c778415
Mike Woofter <mike.woofter@mongodb.com>
2024-04-23 11:53:29 -0500

    refactor aws eks

---
+++ source/includes/authentication/aws-eks.py
@@ -0,0 +1,36 @@
+# start mongoclient
+import os
+from pymongo import MongoClient
+from pymongo.auth_oidc import OIDCCallback, OIDCCallbackContext, OIDCCallbackResult
+
+# define callback, properties, and MongoClient
+class MyCallback(OIDCCallback):
+   def fetch(self, context: OIDCCallbackContext) -> OIDCCallbackResult:
+       with open(os.environ["AWS_WEB_IDENTITY_TOKEN_FILE"]) as fid:
+           token = fid.read()
+       return OIDCCallbackResult(access_token=token)
+properties = {"OIDC_CALLBACK": MyCallback()}
+client = MongoClient(
+   "mongodb://<hostname>:<port>",
+   authMechanism="MONGODB-OIDC",
+   authMechanismProperties=properties,
+)
+# end mongoclient
+
+# start connection string
+import os
+from pymongo import MongoClient
+from pymongo.auth_oidc import OIDCCallback, OIDCCallbackContext, OIDCCallbackResult
+
+# define callback, properties, URI, and MongoClient
+class MyCallback(OIDCCallback):
+   def fetch(self, context: OIDCCallbackContext) -> OIDCCallbackResult:
+       with open(os.environ["AWS_WEB_IDENTITY_TOKEN_FILE"]) as fid:
+           token = fid.read()
+       return OIDCCallbackResult(access_token=token)
+properties = {"OIDC_CALLBACK": MyCallback()}
+uri = ("mongodb://<hostname>:<port>/?"
+       "&authMechanism=MONGODB-OIDC"
+       "&authMechanismProperties=properties")
+client = MongoClient(uri)
+# end connection string
--- source/includes/authentication/gcp-imds.py
+++ source/includes/authentication/gcp-imds.py
@@ -2,12 +2,12 @@
 from pymongo import MongoClient
 
 # define properties and MongoClient
-properties = {"ENVIRONMENT": "azure", "TOKEN_RESOURCE": "<audience>"}
+properties = {"ENVIRONMENT": "gcp", "TOKEN_RESOURCE": "<audience>"}
 client = MongoClient(
-    "mongodb://<hostname>:<port>",
-    username="<Azure identity client ID>",
-    authMechanism="MONGODB-OIDC",
-    authMechanismProperties=properties,
+   "mongodb://<hostname>:<port>",
+   username="<GCP identity client ID>",
+   authMechanism="MONGODB-OIDC",
+   authMechanismProperties=properties,
 )
 # end mongoclient
 
@@ -15,9 +15,9 @@ client = MongoClient(
 from pymongo import MongoClient
 
 # define properties, URI, and MongoClient
-properties = {"ENVIRONMENT": "azure", "TOKEN_RESOURCE": "<audience>"}
+properties = {"ENVIRONMENT": "gcp", "TOKEN_RESOURCE": "<audience>"}
 uri = ("mongodb://<hostname>:<port>/?"
-       "username=<Azure identity client ID>"
+       "username=<GCP identity client ID>"
        "&authMechanism=MONGODB-OIDC"
        "&authMechanismProperties=properties")
 client = MongoClient(uri)
