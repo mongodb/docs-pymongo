18490723cb4af1f41c9080f529681f7d214d8635
norareidy <nora.reidy@mongodb.com>
2024-08-07 16:51:23 -0400

    edit wording

--- source/includes/gridfs/gridfs.py
+++ source/includes/gridfs/gridfs.py
@@ -1,36 +1,40 @@
-# start create bucket
-db = MongoClient().test
-bucket = gridfs.GridFSBucket(db)
-# end create bucket
+    # start create bucket
+    client = MongoClient("<connection string>")
+    db = client["db"]
+    bucket = gridfs.GridFSBucket(db)
+    # end create bucket
 
-# start create custom bucket
-bucket = gridfs.GridFSBucket(db, bucket_name='myCustomBucket')
-# end create custom bucket
+    # start create custom bucket
+    custom_bucket = gridfs.GridFSBucket(db, bucket_name='myCustomBucket')
+    # end create custom bucket
 
-# start upload files
-with open("my_file", "rb") as f:
-    file_id = fs.put(f, filename="my_file")
-# end upload files
+    # start upload files
+    source_path = './file_to_upload'
+    filename = 'my_file'
 
-# start retrieve file info
-for file in bucket.find({}):
-    data = file.read()
-# end retrieve file info
+    with open(source_path, 'rb') as source:
+        bucket.upload_from_stream(filename, source)
+    # end upload files
 
-# start download files name
-destination = open('output_file','wb')
-bucket.download_to_stream_by_name("my_file", destination)
-# end download files name
+    # start retrieve file info
+    for file_doc in bucket.find({}):
+        print(file_doc)
+    # end retrieve file info
 
-# start download files id
-destination = open('output_file','wb+')
-bucket.download_to_stream(file_id, destination)
-# end download files id
+    # start download files name
+    destination = open('output_file','wb')
+    bucket.download_to_stream_by_name('my_file', destination)
+    # end download files name
 
-# start rename files
-fs.rename(file_id, "new_file_name")
-# end rename files
+    # start download files id
+    destination = open('output_file','wb+')
+    bucket.download_to_stream(ObjectId('66b3c86e672a17b6c8a4a4a9'), destination)
+    # end download files id
 
-# start delete files
-bucket.delete(file_id)
-# end delete files
+    # start rename files
+    bucket.rename(ObjectId('66b3c86e672a17b6c8a4a4a9'), "new_file_name")
+    # end rename files
+
+    # start delete files
+    bucket.delete(ObjectId('66b3c86e672a17b6c8a4a4a9'))
+    # end delete files
