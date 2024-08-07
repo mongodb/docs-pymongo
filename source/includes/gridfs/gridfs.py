eef12cf487a037a3c0cd3fc9fb269c52743848fc
norareidy <nora.reidy@mongodb.com>
2024-08-07 17:01:42 -0400

    fixes

--- source/includes/gridfs/gridfs.py
+++ source/includes/gridfs/gridfs.py
@@ -5,15 +5,14 @@
     # end create bucket
 
     # start create custom bucket
-    custom_bucket = gridfs.GridFSBucket(db, bucket_name='myCustomBucket')
+    custom_bucket = gridfs.GridFSBucket(db, bucket_name="myCustomBucket")
     # end create custom bucket
 
     # start upload files
-    source_path = './file_to_upload'
-    filename = 'my_file'
-
-    with open(source_path, 'rb') as source:
-        bucket.upload_from_stream(filename, source)
+    with bucket.open_upload_stream(
+        "my_file", chunk_size_bytes=4,
+        metadata={"contentType": "text/plain"}) as grid_in:
+        grid_in.write("data to store")
     # end upload files
 
     # start retrieve file info
@@ -22,13 +21,13 @@
     # end retrieve file info
 
     # start download files name
-    destination = open('output_file','wb')
-    bucket.download_to_stream_by_name('my_file', destination)
+    file = bucket.open_download_stream_by_name("my_file")
+    contents = file.read()
     # end download files name
 
     # start download files id
-    destination = open('output_file','wb+')
-    bucket.download_to_stream(ObjectId('66b3c86e672a17b6c8a4a4a9'), destination)
+    file = bucket.open_download_stream(ObjectId('66b3c86e672a17b6c8a4a4a9'))
+    contents = file.read()
     # end download files id
 
     # start rename files
