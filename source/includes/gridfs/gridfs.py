206c173ef61e855d0c4a4962ac84c0f6c8b4ee4b
norareidy <nora.reidy@mongodb.com>
2024-08-07 17:06:02 -0400

    reword

--- source/includes/gridfs/gridfs.py
+++ source/includes/gridfs/gridfs.py
@@ -10,7 +10,7 @@
 
     # start upload files
     with bucket.open_upload_stream(
-        "my_file", chunk_size_bytes=4,
+        "my_file", chunk_size_bytes=1048576,
         metadata={"contentType": "text/plain"}) as grid_in:
         grid_in.write("data to store")
     # end upload files
