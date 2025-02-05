bbeaa987a3d79e87e7cfc898e9acb60e5673d6b9
Michael Morisi <michael.morisi@mongodb.com>
2025-02-05 15:26:18 -0500

    NR feedback

--- source/includes/monitoring/monitoring.py
+++ source/includes/monitoring/monitoring.py
@@ -8,19 +8,19 @@ class MyCommandListener(CommandListener):
     def succeeded(self, event: CommandSucceededEvent):
         print(f"Command {event.command_name} succeeded")
 
-    # Other event method implementations here
+    # Include other event method implementations here
 
 class MyServerListener(ServerListener):
     def heartbeat_started(self, event: ServerHeartbeatStartedEvent):
         print(f"Heartbeat started on server with id: {event.connection_id}")
 
-    # Other event method implementations here
+    # Include other event method implementations here
 
 class MyPoolListener(ConnectionPoolListener):
     def connection_created(self, event: ConnectionCreatedEvent):
         print(f"Connection {event.connection_id} created")
 
-    # Other event method implementations here
+    # Include other event method implementations here
 
 listeners = [MyCommandListener(), MyServerListener(), MyPoolListener()]
 client = MongoClient("<connection URI>", event_listeners=listeners)
